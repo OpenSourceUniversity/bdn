db82b6b3cbe9d00dd8b325d38edcb54c8626b753
Jordan Jambazov <jordan.jambazov@os.university>
2018-08-08 11:18:47 +0300

    Add skills AJAX lookup to courses & jobs

--- bdn/course/admin.py
+++ bdn/course/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
 from .models import Course
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(AjaxSelectAdmin):
+
+    form = make_ajax_form(Course, {
+        'skills': 'skills'
+    })
--- bdn/job/admin.py
+++ bdn/job/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
 from .models import Job
 
-admin.site.register(Job)
+
+@admin.register(Job)
+class JobAdmin(AjaxSelectAdmin):
+
+    form = make_ajax_form(Job, {
+        'skills': 'skills'
+    })
