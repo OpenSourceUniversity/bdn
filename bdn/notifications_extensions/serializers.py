a0689656df1b7f1be12df66db6db7eb4cae4ad9c
Jordan Jambazov <jordan.jambazov@os.university>
2018-08-22 15:32:26 +0300

    Expose content type name of all content types fields

--- bdn/notifications_extensions/serializers.py
+++ bdn/notifications_extensions/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
 
 
 class NotificationSerializer(serializers.ModelSerializer):
-    action_object_content_type_interpretation = serializers.\
-        SerializerMethodField('content_type_interpretator')
+    actor_content_type_name = serializers.\
+        SerializerMethodField('_actor_content_type_name')
 
-    def content_type_interpretator(self, obj):
-        return ContentType.objects.get_for_id(
-            id=obj.action_object_content_type.id).name
+    action_object_content_type_name = serializers.\
+        SerializerMethodField('_action_object_content_type_name')
+
+    target_content_type_name = serializers.\
+        SerializerMethodField('_target_content_type_name')
+
+    def _action_object_content_type_name(self, obj):
+        if not obj.action_object_content_type:
+            return None
+        return obj.action_object_content_type.name
+
+    def _actor_content_type_name(self, obj):
+        if not obj.actor_content_type:
+            return None
+        return obj.actor_content_type.name
+
+    def _target_content_type_name(self, obj):
+        if not obj.target_content_type:
+            return None
+        return obj.target_content_type.name
 
     class Meta:
         model = Notification
@@ -17,13 +33,15 @@ class NotificationSerializer(serializers.ModelSerializer):
             'id',
             'level',
             'actor_content_type',
+            'actor_content_type_name',
             'actor_object_id',
             'verb',
             'description',
             'target_content_type',
+            'target_content_type_name',
             'target_object_id',
             'action_object_content_type',
-            'action_object_content_type_interpretation',
+            'action_object_content_type_name',
             'action_object_object_id',
             'timestamp',
             'public',
