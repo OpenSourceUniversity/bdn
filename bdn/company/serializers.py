d446c4e191bfa92fa43c821fc777c1a1975a4912
FrankieRafie <frankierafie@gmail.com>
2018-08-25 20:32:18 +0300

    Provider and Company serializers fix;

--- bdn/company/serializers.py
+++ bdn/company/serializers.py
@@ -3,9 +3,18 @@ from .models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    eth_address = serializers.\
+        SerializerMethodField('_eth_address')
+
+    def _eth_address(self, obj):
+        return obj.user.username
+
     class Meta:
         model = Company
         fields = (
             'id',
             'name',
+            'user',
+            'eth_address',
+            'verified',
         )
--- bdn/provider/serializers.py
+++ bdn/provider/serializers.py
@@ -3,9 +3,18 @@ from .models import Provider
 
 
 class ProviderSerializer(serializers.ModelSerializer):
+    eth_address = serializers.\
+        SerializerMethodField('_eth_address')
+
+    def _eth_address(self, obj):
+        return obj.user.username
+
     class Meta:
         model = Provider
         fields = (
             'id',
             'name',
+            'user',
+            'eth_address',
+            'verified',
         )
