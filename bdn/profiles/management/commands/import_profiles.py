cba6ed2b5ab4493f930e61f6e001a580139ede67
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-08-28 19:38:39 +0300

    Working version of the script generating face Learners, Academias, Businesses and corresponding certificates, courses and job positions

--- bdn/profiles/management/commands/import_profiles.py
+++ bdn/profiles/management/commands/import_profiles.py
@@ -27,9 +27,9 @@ class Command(BaseCommand):
         parser.add_argument('--max_jobs')
 
     def handle(self, *args, **options):
-        countLearners = 5
-        countBusinesses = 5
-        countAcademies = 5
+        countLearners = 10
+        countBusinesses = 10
+        countAcademies = 10
         max_certificates = 0
         max_courses = 0
         max_jobs = 0
@@ -58,118 +58,120 @@ class Command(BaseCommand):
                     eth_wallet += random.choice(HEXADECIMAL_STR).upper()
             user, _ = User.objects.get_or_create(
                 username=eth_wallet)
-            # try:
-            if int(profile) < int(countLearners) and countLearners > 0:
-                profile_obj = Profile.objects.get(user=user)
-                profile_obj.active_profile_type = 1
-                profile_obj.first_name = faker.name().split()[0]
-                profile_obj.last_name = faker.name().split()[1]
-                profile_obj.learner_email = faker.email()
-                profile_obj.learner_position = faker.job()
-                profile_obj.learner_specialisation = "Specialization ..."
-                profile_obj.learner_about = faker.text()
-                profile_obj.learner_country = faker.country()
-                nr_certificates = 0
-                if int(max_certificates) > 0:
-                    nr_certificates = \
-                        int(random_generator.randint(0, int(max_certificates)))
-                    for course in range(0, nr_certificates):
-                        certificate_obj = Certificate.objects.create(
-                            holder=user,
-                            user_eth_address=eth_wallet,
-                            academy_title='Academy ' + faker.company(),
-                            course_title='Course title ' + str(course+1),
-                            learner_eth_address=eth_wallet,
-                            verified=False
-                        )
-                profile_obj.save()
-                print('User eth: ' + str(eth_wallet) + ' was created!')
-                if int(nr_certificates) > 0:
-                    print('--> Certificates added')
-                countLearners -= 1
-            elif int(profile) < (fake_users-int(countAcademies)) and countBusinesses > 0:
-                business_obj = Profile.objects.get(user=user)
-                company, _ = Company.objects.get_or_create(user=user)
-                business_obj.active_profile_type = 3
-                business_obj.company_name = faker.company()
-                business_obj.company_website = 'https://www.business.com'
-                business_obj.company_email = faker.email()
-                business_obj.company_country = faker.country()
-                if random.choice('01') == '0':
-                    business_obj.company_verified = True
-                else:
-                    business_obj.company_verified = False
-                nr_jobs = 0
-                if int(max_jobs) > 0:
+            try:
+                if int(profile) < int(countLearners) and countLearners > 0:
+                    profile_obj = Profile.objects.get(user=user)
+                    profile_obj.active_profile_type = 1
+                    profile_obj.first_name = faker.name().split()[0]
+                    profile_obj.last_name = faker.name().split()[1]
+                    profile_obj.learner_email = faker.email()
+                    profile_obj.learner_position = faker.job()
+                    profile_obj.learner_specialisation = "Specialization ..."
+                    profile_obj.learner_about = faker.text()
+                    profile_obj.learner_country = faker.country()
+                    nr_certificates = 0
+                    if int(max_certificates) > 0:
+                        nr_certificates = \
+                            int(random_generator.randint(0,
+                                int(max_certificates)))
+                        for course in range(0, nr_certificates):
+                            certificate_obj = Certificate.objects.create(
+                                holder=user,
+                                user_eth_address=eth_wallet,
+                                academy_title='Academy ' + faker.company(),
+                                course_title='Course title ' + str(course+1),
+                                learner_eth_address=eth_wallet,
+                                verified=False
+                            )
+                    profile_obj.save()
+                    print('User eth: ' + str(eth_wallet) + ' was created!')
+                    if int(nr_certificates) > 0:
+                        print('--> Certificates added')
+                    countLearners -= 1
+                elif int(profile) < (fake_users-int(countAcademies)) and \
+                        countBusinesses > 0:
+                    business_obj = Profile.objects.get(user=user)
+                    company, _ = Company.objects.get_or_create(user=user)
+                    business_obj.active_profile_type = 3
+                    business_obj.company_name = faker.company()
+                    business_obj.company_website = 'https://www.business.com'
+                    business_obj.company_email = faker.email()
+                    business_obj.company_country = faker.country()
+                    if random.choice('01') == '0':
+                        business_obj.company_verified = True
+                    else:
+                        business_obj.company_verified = False
+                    nr_jobs = 0
+                    if int(max_jobs) > 0:
+                        industry, _ = Industry.objects.get_or_create(
+                            name='Animation')
+                        nr_jobs = \
+                            int(random_generator.randint(0, int(max_jobs)))
+                        for job in range(0, nr_jobs):
+                            job_obj = Job.objects.create(
+                                company=company,
+                                title=faker.job(),
+                                location='Remote',
+                                overview=faker.text(),
+                                description=faker.text(),
+                                hours='12',
+                                languages=['English']
+                            )
+                            job_obj.industries.add(industry)
+                            nr_skills = \
+                                int(random_generator.randint(1, len(SKILLS)))
+                            for skill_id in range(1, nr_skills):
+                                skill, _ = Skill.objects.get_or_create(
+                                    name=SKILLS[skill_id]
+                                )
+                                job_obj.skills.add(skill)
+                    business_obj.save()
+                    print('Business eth: ' + str(eth_wallet) + ' was created!')
+                    if nr_jobs > 0:
+                        print('--> Jobs added')
+                    countBusinesses -= 1
+                elif int(profile) < fake_users and countAcademies > 0:
+                    academy_obj = Profile.objects.get(user=user)
+                    academy_obj.active_profile_type = 2
+                    academy_obj.academy_name = faker.company()
+                    provider, _ = Provider.objects.get_or_create(
+                        name=academy_obj.academy_name)
                     industry, _ = Industry.objects.get_or_create(
-                        name='Animation')
-                    nr_jobs = \
-                        int(random_generator.randint(0, int(max_jobs)))
-                    for job in range(0, nr_jobs):
-                        job_obj = Job.objects.create(
-                            company=company,
-                            title=faker.job(),
-                            location='Remote',
-                            overview=faker.text(),
-                            description=faker.text(),
-                            hours='12',
-                            languages=['English']
-                        )
-                        job_obj.industries.add(industry)
-                        nr_skills = \
-                            int(random_generator.randint(1, len(SKILLS)))
-                        for skill_id in range(1, nr_skills):
-                            skill, _ = Skill.objects.get_or_create(
-                                name=SKILLS[skill_id]
+                        name='Accounting')
+                    academy_obj.academy_website = 'https://www.academy.com'
+                    academy_obj.academy_email = faker.email()
+                    academy_obj.academy_country = faker.country()
+                    if random.choice('01') == '0':
+                        academy_obj.academy_verified = True
+                    else:
+                        academy_obj.academy_verified = False
+                    nr_courses = 0
+                    if int(max_courses) > 0:
+                        nr_courses = \
+                            int(random_generator.randint(0, int(max_courses)))
+                        for _ in range(0, nr_courses):
+                            course_obj = Course.objects.create(
+                                title='Course titl',
+                                description=faker.text(),
+                                external_link='https://www.coursera.org/',
+                                provider=provider,
+                                tutor=academy_obj.academy_name
                             )
-                            job_obj.skills.add(skill)
-                business_obj.save()
-                print('Business eth: ' + str(eth_wallet) + ' was created!')
-                if nr_jobs > 0:
-                    print('--> Jobs added')
-                countBusinesses -= 1
-            elif int(profile) < fake_users and countAcademies > 0:
-                academy_obj = Profile.objects.get(user=user)
-                academy_obj.active_profile_type = 2
-                academy_obj.academy_name = faker.company()
-                provider, _ = Provider.objects.get_or_create(
-                    name=academy_obj.academy_name)
-                industry, _ = Industry.objects.get_or_create(
-                    name='Accounting')
-                academy_obj.academy_website = 'https://www.academy.com'
-                academy_obj.academy_email = faker.email()
-                academy_obj.academy_country = faker.country()
-                if random.choice('01') == '0':
-                    academy_obj.academy_verified = True
-                else:
-                    academy_obj.academy_verified = False
-                nr_courses = 0
-                if int(max_courses) > 0:
-                    nr_courses = \
-                        int(random_generator.randint(0, int(max_courses)))
-                    for _ in range(0, nr_courses):
-                        course_obj = Course.objects.create(
-                            title='Course titl',
-                            description=faker.text(),
-                            external_link='https://www.coursera.org/',
-                            provider=provider,
-                            tutor=academy_obj.academy_name
-                        )
-                        course_obj.industries.add(industry)
-                        nr_skills = \
-                            int(random_generator.randint(1, len(SKILLS)))
-                        for skill_id in range(1, nr_skills):
-                            skill, _ = Skill.objects.get_or_create(
-                                name=SKILLS[skill_id]
-                            )
-                            course_obj.skills.add(skill)
-                academy_obj.save()
-                print('Academy eth: ' + str(eth_wallet) + ' was created!')
-                if nr_courses > 0:
-                    print('--> Courses added')
-                countAcademies -= 1
-            # except Exception:
-            #     continue
+                            course_obj.industries.add(industry)
+                            nr_skills = \
+                                int(random_generator.randint(1, len(SKILLS)))
+                            for skill_id in range(1, nr_skills):
+                                skill, _ = Skill.objects.get_or_create(
+                                    name=SKILLS[skill_id]
+                                )
+                                course_obj.skills.add(skill)
+                    academy_obj.save()
+                    print('Academy eth: ' + str(eth_wallet) + ' was created!')
+                    if nr_courses > 0:
+                        print('--> Courses added')
+                    countAcademies -= 1
+            except Exception:
+                continue
         print('Generation of face profiles finished successfully!')
         print(str(Profile.objects.count()) + ' profiles stored in the DB')
         return
