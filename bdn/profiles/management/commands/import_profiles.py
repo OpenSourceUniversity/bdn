b6dcaac157cc4cdbb6f70880a93b6df99880cd72
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-08-28 13:49:34 +0300

    Adding --max_courses option and attaching fake courses to fake academies

--- bdn/profiles/management/commands/import_profiles.py
+++ bdn/profiles/management/commands/import_profiles.py
@@ -8,9 +8,10 @@ from bdn.company.models import Company
 from bdn.auth.models import User
 from faker import Faker
 from bdn.profiles.models import Profile
-from random import choice
+import random
 
 HEXADECIMAL_STR = '0123456789abcdef'
+SKILLS = ['Python', 'Django', 'Java', '.NET', 'Project Management', 'Design']
 
 
 class Command(BaseCommand):
@@ -27,7 +28,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         countLearners = 1
         countBusinesses = 1
-        countAcademies = 1
+        countAcademies = 5
+        max_certificates = 0
+        max_courses = 0
+        max_jobs = 0
         if options['learners']:
             countLearners = int(options['learners'])
         if options['businesses']:
@@ -43,18 +47,19 @@ class Command(BaseCommand):
         fake_users = int(countLearners) + int(countBusinesses) + \
             int(countAcademies)
         faker = Faker()
+        random_generator = random.SystemRandom()
         for profile in range(fake_users):
             eth_wallet = '0x'
             for _ in range(40):
-                if choice('01') == '0':
-                    eth_wallet += choice(HEXADECIMAL_STR).lower()
+                if random.choice('01') == '0':
+                    eth_wallet += random.choice(HEXADECIMAL_STR).lower()
                 else:
-                    eth_wallet += choice(HEXADECIMAL_STR).upper()
+                    eth_wallet += random.choice(HEXADECIMAL_STR).upper()
             user, _ = User.objects.get_or_create(
                 username=eth_wallet)
             try:
-                profile_obj = Profile.objects.get(user=user)
                 if int(profile) < int(countLearners):
+                    profile_obj = Profile.objects.get(user=user)
                     profile_obj.active_profile_type = 1
                     profile_obj.first_name = faker.name().split()[0]
                     profile_obj.last_name = faker.name().split()[1]
@@ -73,7 +78,7 @@ class Command(BaseCommand):
                     business_obj.company_website = 'https://www.business.com'
                     business_obj.company_email = faker.email()
                     business_obj.company_country = faker.country()
-                    if choice('01') == '0':
+                    if random.choice('01') == '0':
                         business_obj.company_verified = True
                     else:
                         business_obj.company_verified = False
@@ -81,20 +86,44 @@ class Command(BaseCommand):
                     print('Business eth: ' + str(eth_wallet) + ' was created!')
                 elif int(profile) < fake_users:
                     academy_obj = Profile.objects.get(user=user)
-                    provider, _ = Provider.objects.get_or_create(user=user)
                     academy_obj.active_profile_type = 2
                     academy_obj.academy_name = faker.company()
+                    provider, _ = Provider.objects.get_or_create(
+                        name=academy_obj.academy_name)
+                    industry, _ = Industry.objects.get_or_create(
+                        name='Accounting')
                     academy_obj.academy_website = 'https://www.academy.com'
                     academy_obj.academy_email = faker.email()
                     academy_obj.academy_country = faker.country()
-                    if choice('01') == '0':
+                    if random.choice('01') == '0':
                         academy_obj.academy_verified = True
                     else:
                         academy_obj.academy_verified = False
+                    if int(max_courses) > 0:
+                        nr_courses = \
+                            int(random_generator.randint(0, int(max_courses)))
+                        for _ in range(0, nr_courses):
+                            course_obj = Course.objects.create(
+                                title='Course titl',
+                                description=faker.text(),
+                                external_link='https://www.coursera.org/',
+                                provider=provider,
+                                tutor=academy_obj.academy_name
+                            )
+                            course_obj.industries.add(industry)
+                            nr_skills = \
+                                int(random_generator.randint(1, len(SKILLS)))
+                            for skill_id in range(1, nr_skills):
+                                skill, _ = Skill.objects.get_or_create(
+                                    name=SKILLS[skill_id]
+                                )
+                                course_obj.skills.add(skill)
                     academy_obj.save()
                     print('Academy eth: ' + str(eth_wallet) + ' was created!')
+                    if nr_courses > 0:
+                        print('--> Courses added')
             except Exception:
                 continue
         print('Generation of face profiles finished successfully!')
-        print(str(Profile.objects.count()) + 'profiles stored in the DB')
+        print(str(Profile.objects.count()) + ' profiles stored in the DB')
         return
