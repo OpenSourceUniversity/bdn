013169b6d47e710aa453f5658d91ac06f966a86d
Jordan Jambazov <jordan.jambazov@os.university>
2018-09-04 19:16:45 +0300

    Refactoring and covering with tests course views

---
+++ bdn/course/tests.py
@@ -0,0 +1,140 @@
+# flake8: noqa
+
+import uuid
+from django.test import RequestFactory, TestCase
+from bdn.auth.models import User
+from bdn.provider.models import Provider
+from .views import CourseViewSet
+
+
+class CourseTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_course_create_and_then_delete_it(self):
+        # Create course with error (provider not found)
+        request = self.factory.post(
+            '/api/v1/courses/',
+            data={
+                'title': 'test',
+                'description': 'test',
+                'external_link': 'http://example.com/',
+            },
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735'
+        )
+        response = CourseViewSet.as_view({'post': 'create'})(request)
+        self.assertEqual(response.status_code, 400)
+
+        # Create course
+        eth_address = '0xD2BE64317Eb1832309DF8c8C18B09871809f3735'.lower()
+        user, _ = User.objects.get_or_create(username=eth_address)
+        provider = Provider(user=user, name='test')
+        provider.save()
+        request = self.factory.post(
+            '/api/v1/courses/',
+            data={
+                'title': 'test',
+                'description': 'test',
+                'external_link': 'http://example.com/',
+            },
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735'
+        )
+        response = CourseViewSet.as_view({'post': 'create'})(request)
+        self.assertEqual(response.status_code, 200)
+        course_pk = str(response.data['pk'])
+
+        # Create wrong course (invalid data)
+        request = self.factory.post(
+            '/api/v1/courses/',
+            data={
+                'title': '',
+                'description': 'test',
+                'external_link': 'http://example.com/',
+            },
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735'
+        )
+        response = CourseViewSet.as_view({'post': 'create'})(request)
+        self.assertEqual(response.status_code, 400)
+
+        # Retrieve the course
+        request = self.factory.get(
+            '/api/v1/courses/{}/'.format(course_pk),
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        response = CourseViewSet.as_view({'get': 'retrieve'})(request, pk=course_pk)
+        self.assertEqual(response.status_code, 200)
+
+        wrong_pk = str(uuid.uuid4())
+        request = self.factory.get(
+            '/api/v1/courses/{}/'.format(wrong_pk),
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'retrieve'})
+        response = view(request, pk=wrong_pk)
+        self.assertEqual(response.status_code, 404)
+
+        # Listing
+        industry_pks = '1427837e-babf-42c8-98ce-3517cb768249|some_wrong_uuid||'
+        request = self.factory.get(
+            '/api/v1/courses/?filter_industry={}&is_featured=1'.format(industry_pks),
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Search
+        request = self.factory.get(
+            '/api/v1/courses/?q=python',
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+        request = self.factory.get(
+            '/api/v1/courses/search/?q=python',
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'search'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+        request = self.factory.get(
+            '/api/v1/courses/autocomplete/?q=autocomplete',
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'autocomplete'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Get course by id
+        request = self.factory.get(
+            '/api/v1/courses/{}/get_by_id/'.format(course_pk),
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'get_by_id'})
+        response = view(request, pk=course_pk)
+        self.assertEqual(response.status_code, 200)
+
+        wrong_pk = str(uuid.uuid4())
+        request = self.factory.get(
+            '/api/v1/courses/{}/get_by_id/'.format(wrong_pk),
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'get': 'get_by_id'})
+        response = view(request, pk=wrong_pk)
+        self.assertEqual(response.status_code, 404)
+
+        # Delete it
+        url = '/api/v1/courses/{}/delete_by_id/'.format(course_pk)
+        request = self.factory.post(
+            url,
+            HTTP_AUTH_SIGNATURE='0xe646de646dde9cee6875e3845428ce6fc13d41086e8a7f6531d1d526598cc4104122e01c38255d1e1d595710986d193f52e3dbc47cb01cb554d8e4572d6920361c',
+            HTTP_AUTH_ETH_ADDRESS='D2BE64317Eb1832309DF8c8C18B09871809f3735')
+        view = CourseViewSet.as_view({'post': 'delete_by_id'})
+        response = view(request, pk=course_pk)
+        self.assertEqual(response.status_code, 200)
--- bdn/course/views.py
+++ bdn/course/views.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 from django.db.models import Q
-from rest_framework import status, viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets, mixins
 from rest_framework.decorators import list_route, detail_route
 from bdn.auth.models import User
 from rest_framework.pagination import LimitOffsetPagination
@@ -9,7 +10,6 @@ from rest_framework.response import Response
 from haystack.query import SearchQuerySet
 from bdn.auth.signature_authentication import SignatureAuthentication
 from bdn.auth.utils import get_auth_eth_address
-from bdn.profiles.models import Profile
 from bdn.industry.models import Industry
 from bdn.skill.models import Skill
 from bdn.provider.models import Provider
@@ -18,39 +18,20 @@ from .serializers import CourseSerializer
 from .models import Course
 
 
-class CourseViewSet(viewsets.ModelViewSet):
+class CourseViewSet(mixins.RetrieveModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     pagination_class = LimitOffsetPagination
     authentication_classes = (SignatureAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def update(self, request):
-        return Response({
-                    'status': 'denied'}, status=status.HTTP_401_UNAUTHORIZED)
-
-    def partial_update(self, request):
-        return Response({
-                    'status': 'denied'}, status=status.HTTP_401_UNAUTHORIZED)
-
-    def destroy(self, request):
-        return Response({
-                    'status': 'denied'}, status=status.HTTP_401_UNAUTHORIZED)
-
     def retrieve(self, request, pk=None):
-        try:
-            course = Course.objects.get(id=pk)
-        except Course.DoesNotExist:
-            return Response({
-                'error': 'Course not found',
-            }, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            user = User.objects.get(username=course.provider.user)
-        except User.DoesNotExist:
-            return Response({
-                'error': 'User not found',
-            }, status=status.HTTP_400_BAD_REQUEST)
-        profile = Profile.objects.get(user=user)
+        course = get_object_or_404(Course, id=pk)
+        user = course.provider.user
+        profile = user.profile
         serializerProfile = AcademyProfileSerializer(profile)
         serializerCourse = CourseSerializer(course)
         return Response({
@@ -65,6 +46,7 @@ class CourseViewSet(viewsets.ModelViewSet):
             qs = [
                 _.object
                 for _ in sqs
+                if _.object is not None
             ]
             return qs
 
@@ -82,8 +64,7 @@ class CourseViewSet(viewsets.ModelViewSet):
                 UUID(filtered_industry_id, version=4)
             except ValueError:
                 continue
-            if filtered_industry_id:
-                industry_filter |= Q(industries__id=filtered_industry_id)
+            industry_filter |= Q(industries__id=filtered_industry_id)
         return industry_filter
 
     def featured_filter(self):
@@ -96,7 +77,9 @@ class CourseViewSet(viewsets.ModelViewSet):
     def search(self, request):
         query = self.request.GET.get('q', '')
         sqs = SearchQuerySet().filter(title=query).models(Course)
-        serializer = self.get_serializer([s.object for s in sqs], many=True)
+        serializer = self.get_serializer([
+            s.object for s in sqs if s.object is not None
+        ], many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @list_route(methods=['get'])
@@ -105,7 +88,8 @@ class CourseViewSet(viewsets.ModelViewSet):
         sqs = SearchQuerySet()\
             .filter(title_auto=request.GET.get('q', ''))\
             .models(Course)[:AUTOCOMPLETE_SIZE]
-        serializer = self.get_serializer([s.object for s in sqs], many=True)
+        serializer = self.get_serializer([
+            s.object for s in sqs if s.object is not None], many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @list_route(methods=['get'])
@@ -125,31 +109,14 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['get'])
     def get_by_id(self, request, pk=None):
-        eth_address = get_auth_eth_address(request.META)
-        course_id = pk
-        try:
-            course = Course.objects.get(id=course_id)
-        except Course.DoesNotExist:
-            return Response({
-                'error': 'Course not found',
-            }, status=status.HTTP_400_BAD_REQUEST)
-        if course.provider.user.username == eth_address:
-            serializer = self.get_serializer(course)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                'status': 'denied'}, status=status.HTTP_401_UNAUTHORIZED)
+        course = get_object_or_404(Course, id=pk)
+        serializer = self.get_serializer(course)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @detail_route(methods=['post'])
     def edit_by_id(self, request, pk=None):
         eth_address = get_auth_eth_address(request.META)
-        course_id = pk
-        try:
-            course = Course.objects.get(id=course_id)
-        except Course.DoesNotExist:
-            return Response({
-                'error': 'Course not found',
-            }, status=status.HTTP_400_BAD_REQUEST)
+        course = get_object_or_404(Course, id=pk)
         if course.provider.user.username == eth_address:
             serializer = self.get_serializer(
                 data=request.data, instance=course, partial=True)
@@ -162,27 +129,26 @@ class CourseViewSet(viewsets.ModelViewSet):
             'status': 'denied'}, status=status.HTTP_401_UNAUTHORIZED)
 
     def create(self, request, pk=None):
-        eth_address = get_auth_eth_address(request.META)
-        try:
-            academy = User.objects.get(username=eth_address)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        academy = request.user
         try:
             provider = Provider.objects.get(user=academy)
         except Provider.DoesNotExist:
             return Response({
                 'error': 'Provider not found',
             }, status=status.HTTP_400_BAD_REQUEST)
-        skills_post = request.data.get('skills')
+        skills_post = request.data.get('skills', [])
         skills_lower = [_.lower() for _ in skills_post]
         skills = Skill.objects.filter(name__in=skills_lower)
         industries = Industry.objects.filter(
-            name__in=request.data.get('industries'))
+            name__in=request.data.get('industries', []))
         serializer = CourseSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(
+            course = serializer.save(
                 provider=provider, industries=industries, skills=skills)
-            return Response({'status': 'ok'})
+            return Response({
+                'status': 'ok',
+                'pk': course.pk,
+            })
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
@@ -190,13 +156,8 @@ class CourseViewSet(viewsets.ModelViewSet):
     @detail_route(methods=['post'])
     def delete_by_id(self, request, pk=None):
         eth_address = get_auth_eth_address(request.META)
-        course_id = pk
-        try:
-            course = Course.objects.get(id=course_id)
-        except Course.DoesNotExist:
-            return Response({
-                'error': 'Course not found',
-            }, status=status.HTTP_400_BAD_REQUEST)
+        course = get_object_or_404(Course, id=pk)
+
         if course.provider.user.username == eth_address:
             course.delete()
             return Response({'status': 'ok'})
