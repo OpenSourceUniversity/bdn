5576073d0f7e4c565c7fbbf114a619459e089e31
Jordan Jambazov <jordan.jambazov@os.university>
2018-09-04 17:27:38 +0300

    Ignore coverage on apps.py & cover company serializer

--- bdn/company/tests.py
+++ bdn/company/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
+from .serializers import CompanySerializer
 
 
 class CompanyTests(TestCase):
-    pass
+    def test_company_eth_address(self):
+        class FakeCompany:
+            class user:
+                username = '0x0'
+        serializer = CompanySerializer()
+        result = serializer._eth_address(FakeCompany)
+        self.assertEqual(result, '0x0')
