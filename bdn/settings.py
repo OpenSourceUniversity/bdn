21823834f8c1e5890ba870b6094d9559b7010fcc
Jordan Jambazov <jordan.jambazov@os.university>
2018-10-05 21:24:40 +0300

    Do not be strict to the manifest in whitenoise

--- bdn/settings.py
+++ bdn/settings.py
@@ -239,7 +239,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'bdn.storage.WhiteNoiseStaticFilesStorage'
 
 # Email settings
 EMAIL_HOST = 'smtp'
