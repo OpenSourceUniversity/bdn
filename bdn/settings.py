77012d792a74f618eb7a0c20aa8f7a5b2904ddd8
FrankieRafie <frankierafie@gmail.com>
2018-11-05 20:48:43 +0200

    Turn off HAYSTACK_SIGNAL_PROCESSOR;

--- bdn/settings.py
+++ bdn/settings.py
@@ -160,7 +160,7 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+# HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 
 # Django notifications
